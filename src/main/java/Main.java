3b1c767cfe3c3cee0bdba91aac679c91690885f7
Gabriel Malosto <professional@gabdumal.com>
2024-11-26 02:42:01 -0300

    Create ServiceFactory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
-package com.gabdumal;
+/*
+ * Copyright (c) 2024.
+ *
+ * Licensed under the GNU Affero General Public License, Version 3.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at <https://www.gnu.org/licenses/agpl-3.0.txt>.
+ */
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
---
+++ src/main/java/creational_patterns/factory_method/ServiceFactory.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024.
+ *
+ * Licensed under the GNU Affero General Public License, Version 3.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at <https://www.gnu.org/licenses/agpl-3.0.txt>.
+ */
+
+package creational_patterns.factory_method;
+
+import static java.lang.Class.forName;
+
+public class ServiceFactory {
+
+    public static IService getService(String serviceName) {
+        Class newClass = null;
+        Object newObject = null;
+
+        try {
+            newClass = forName("creational_patterns.factory_method.Service" + serviceName);
+            newObject = newClass.newInstance();
+        } catch (Exception exception) {
+            throw new IllegalArgumentException("No such service: " + serviceName);
+        }
+
+        if (!(newObject instanceof IService)) {
+            throw new IllegalArgumentException("Invalid service: %s".formatted(serviceName));
+        }
+
+        return (IService) newObject;
+    }
+
+}
