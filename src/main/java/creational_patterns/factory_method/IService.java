c326ac40c0f43d437bdd5daa2823b1e54b2d6e94
Gabriel Malosto <professional@gabdumal.com>
2024-11-26 02:41:45 -0300

    Create interface IService

---
+++ src/main/java/creational_patterns/factory_method/IService.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024.
+ *
+ * Licensed under the GNU Affero General Public License, Version 3.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at <https://www.gnu.org/licenses/agpl-3.0.txt>.
+ */
+
+package creational_patterns.factory_method;
+
+public interface IService {
+    String run();
+
+    String cancel();
+}
