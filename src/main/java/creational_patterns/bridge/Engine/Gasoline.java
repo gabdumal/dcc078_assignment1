98e7f09c4c81b325c1853bf72a60bfa849f982e0
Gabriel Malosto <professional@gabdumal.com>
2024-12-17 22:28:11 -0300

    Create classes that implement Engine following Bridge creational pattern

---
+++ src/main/java/creational_patterns/bridge/Engine/Gasoline.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024 Gabriel Malosto.
+ *
+ * Licensed under the GNU Affero General Public License, Version 3.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at <https://www.gnu.org/licenses/agpl-3.0.txt>.
+ */
+
+package creational_patterns.bridge.Engine;
+
+public class Gasoline implements Engine {
+
+    @Override
+    public double costIncreasePercentage() {
+        return 0.075d;
+    }
+    
+}
